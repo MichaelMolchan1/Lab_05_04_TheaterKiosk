08918563b898fbaca2139b799105bd393de16a11
Molch <molchaml@mail.uc.edu>
2022-09-22 14:55:22 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 45;
+        if (age >= 21)
+        {
+            System.out.println("Here is your wrist band enjoy your movie!");
+        }
+    }
+}
